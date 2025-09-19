a8fc3934fd1f1fa458c049074cc58339ccad3186
Mou <motaz.farajalla@gmail.com>
2025-09-19 20:21:19 +0300

    update cost pool

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ const port = process.env.PORT || 3000;
 
 // DB connection (Coolify injects env variables)
 const pool = new Pool({
-  host: process.env.DB_HOST || "voc0oocw844s0wk80coo4og8",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "123456",
-  database: process.env.DB_NAME || "postgres",
+ // host: process.env.DB_HOST || "voc0oocw844s0wk80coo4og8",
+ // port: process.env.DB_PORT || 5432,
+ // user: process.env.DB_USER || "postgres",
+ // password: process.env.DB_PASSWORD || "123456",
+ // database: process.env.DB_NAME || "postgres",
+
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.get("/", async (req, res) => {
